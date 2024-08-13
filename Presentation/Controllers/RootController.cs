9e0c92b58def621e2bad819869fbd48583ace1ec
Alper Yusuf Dursun <AlperYusuf.Dursun@aydinli.com.tr>
2024-08-13 11:13:32 +0300

    added root documentation

---
+++ Presentation/Controllers/RootController.cs
@@ -0,0 +1,50 @@
+﻿using Entities.LinkModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class RootController : ControllerBase
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        public RootController(LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet(Name = "GetRootAsync")]
+        public async Task<IActionResult> GetRootAsync([FromHeader(Name = "Accept")]string mediaType)
+        {
+            if (mediaType.Contains("application/vnd.btkakademi.apiroot"))
+            {
+                var list = new List<Link>()
+                {
+                    new Link()
+                    {
+                        Href = _linkGenerator.GetUriByName(HttpContext, nameof(GetRootAsync), new{}),
+                        Rel = "_self",
+                        Method = "GET"
+                    },
+                    new Link()
+                    {
+                        Href = _linkGenerator.GetUriByName(HttpContext, nameof(BooksController.GetAllBooksAsync), new{}),
+                        Rel = "books",
+                        Method = "GET"
+                    },
+                    new Link()
+                    {
+                        Href = _linkGenerator.GetUriByName(HttpContext, nameof(BooksController.CreateOneBookAsync), new{}),
+                        Rel = "books",
+                        Method = "POST"
+                    }
+                };
+
+                return Ok(list);
+            }
+            return NoContent(); // 204
+        }
+    }
+}
