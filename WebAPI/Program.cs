da66149b6ff103ba826809ecff2bfc7043a1953c
Alper Yusuf Dursun <AlperYusuf.Dursun@aydinli.com.tr>
2024-08-13 14:53:12 +0300

    added rate limiting functionality.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NLog;
@@ -45,6 +46,9 @@ builder.Services.AddScoped<IBookLinks, BookLinks>();
 builder.Services.ConfigureVersioning();
 builder.Services.ConfigureResponseCaching();
 builder.Services.ConfigureHttpCacheHeaders();
+builder.Services.AddMemoryCache();
+builder.Services.ConfigureRateLimitingOptions();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
@@ -65,6 +69,7 @@ if (app.Environment.IsProduction())
 
 app.UseHttpsRedirection();
 
+app.UseIpRateLimiting(); // Microsoft, Rate Limiting'in Cors'dan önce çaðýrýlmasýný öneriyor.
 app.UseCors("CorsPolicy");
 
 app.UseResponseCaching(); // Microsoft Cors'dan sonra Caching ifadelerinin çaðýrýlmasýný öneriyor.
